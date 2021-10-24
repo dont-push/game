25235a854dacf7d063924559c0e83f9bb9c0005a
vadim.malovaniy <vadim.malovaniy@faifly.com>
2021-10-25 01:50:05 +0300

    install taiga ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiNotificationsModule } from "@taiga-ui/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TuiMarkerIconModule } from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    TuiRootModule,
+    BrowserAnimationsModule,
+    TuiDialogModule,
+    TuiNotificationsModule,
+    TuiMarkerIconModule,
+],
   providers: [],
   bootstrap: [AppComponent]
 })
